d18450e7f64a471f87871439faf8aeed08d990a1
NHBKhang <nhbkhang12@gmail.com>
2025-03-15 18:18:50 +0700

    Update project

---
+++ Hibernate/src/main/java/com/nhbk/hibernate/HibernateUtils.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nhbk.hibernate;
+
+/**
+ *
+ * @author KHANG
+ */
+public class HibernateUtils {
+    
+}
